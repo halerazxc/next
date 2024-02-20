f7bffc44800e311808aae0f324a4b9047608c684
halerazxc <125389310+halerazxc@users.noreply.github.com>
2024-02-20 20:38:56 +0400

    update

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -8,12 +8,10 @@ const ThemeToggle = () => {
   const { theme, setTheme } = useTheme()
 
   return (
-    <>
-      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-        <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-75 dark:-rotate-90 dark:scale-0' />
-        <MoonIcon className='h-[1.2rem] w-[1.2rem] absolute rotate-90 scale-0 transition-all duration-75 dark:rotate-0 dark:scale-100' />
-      </Button>
-    </>
+    <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-200 dark:-rotate-90 dark:scale-0' />
+      <MoonIcon className='h-[1.2rem] w-[1.2rem] absolute rotate-90 scale-0 transition-all duration-200 dark:rotate-0 dark:scale-100' />
+    </Button>
   )
 }
 
