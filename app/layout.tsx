fca55b88a90ffa8b5af399ea5ea646580f6afc3e
halerazxc <125389310+halerazxc@users.noreply.github.com>
2024-02-20 17:57:57 +0400

    added header / footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/ui/Header'
+import Footer from '@/components/Footer'
 
 const inter = Montserrat({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
---
+++ components/ui/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header>This Is Header</header>
+    </>
+  )
+}
+
+export default Header
