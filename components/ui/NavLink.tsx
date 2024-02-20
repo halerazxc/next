65da0bdc4f7f60cf621bd52fe94d7e0f2145df05
halerazxc <125389310+halerazxc@users.noreply.github.com>
2024-02-20 20:38:17 +0400

    add active navLink

--- components/ui/NavLink.tsx
+++ components/ui/NavLink.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 interface NavLinkProps {
   id: string
@@ -7,11 +10,20 @@ interface NavLinkProps {
   src: string
 }
 
-const NavLinkItem: React.FC<NavLinkProps> = ({ id, name, src }) => {
-  return <Link href={src}>{name}</Link>
+const NavLinkItem = ({ id, name, src }: NavLinkProps) => {
+  const pathname = usePathname()
+  const isActive = pathname === src
+  return (
+    <Link
+      href={src}
+      className={`nav-link ${isActive ? ' text-[#884dee]' : ''}`}
+    >
+      {name}
+    </Link>
+  )
 }
 
-const NavLink = () => {
+const NavLink = ({ hiddenProp }: { hiddenProp: string }) => {
   const navigateLinks = [
     { id: '29', name: 'Home', src: '/' },
     { id: '3', name: 'Project', src: '/project' },
@@ -19,7 +31,7 @@ const NavLink = () => {
   ]
 
   return (
-    <nav className='flex items-center gap-x-2 text-[16px]'>
+    <nav className={`flex items-center gap-x-4 text-[16px] ${hiddenProp}`}>
       {navigateLinks.map(link => (
         <React.Fragment key={link.id}>
           <NavLinkItem {...link} />
