56ec294bfc401a1df45f7384f4a8fbcf1bac26d2
halerazxc <125389310+halerazxc@users.noreply.github.com>
2024-02-20 19:00:25 +0400

    refactoring code

--- components/ui/NavLink.tsx
+++ components/ui/NavLink.tsx
@@ -1,34 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
 
-const navigateLink = [
-  {
-    id: '29',
-    name: 'Home',
-    src: '/'
-  },
-  {
-    id: '3',
-    name: 'Project',
-    src: '/project'
-  },
-  {
-    id: '19',
-    name: 'Contact',
-    src: '/contact'
-  }
-]
+interface NavLinkProps {
+  id: string
+  name: string
+  src: string
+}
+
+const NavLinkItem: React.FC<NavLinkProps> = ({ id, name, src }) => {
+  return <Link href={src}>{name}</Link>
+}
 
 const NavLink = () => {
+  const navigateLinks = [
+    { id: '29', name: 'Home', src: '/' },
+    { id: '3', name: 'Project', src: '/project' },
+    { id: '19', name: 'Contact', src: '/contact' }
+  ]
+
   return (
-    <nav>
-      {navigateLink.map(link => {
-        return (
-          <Link key={link.id} href={link.src}>
-            {link.name}
-          </Link>
-        )
-      })}
+    <nav className='flex items-center gap-x-2 text-[16px]'>
+      {navigateLinks.map(link => (
+        <React.Fragment key={link.id}>
+          <NavLinkItem {...link} />
+        </React.Fragment>
+      ))}
     </nav>
   )
 }
